5ea9c411eac00c49dd85c0f1a633a9e5cef48fb1
lipeehmartins <lipihmartins@gmail.com>
2021-04-25 00:45:46 -0400

    nav-bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar'
 import './App.css';
 
 function App() {
   return (
-   <h2>Portfolio</h2>
+   <Navbar />
   );
 }
 
